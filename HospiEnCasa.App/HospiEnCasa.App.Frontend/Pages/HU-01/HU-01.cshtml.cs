78e42c35a552256b544572428675570385ae4da8
T2 John Betancur Perez <betancurtperez.6-t2@teleperformance.co>
2022-07-17 16:33:37 -0500

    Se realiza conexión con el backend

--- HospiEnCasa.App/HospiEnCasa.App.Frontend/Pages/HU-01/HU-01.cshtml.cs
+++ HospiEnCasa.App/HospiEnCasa.App.Frontend/Pages/HU-01/HU-01.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Persistencia;
+using HospiEnCasa.App.Dominio;
 
-namespace HolaWeb.App.Frontend.Pages
+namespace HospiEnCasa.App.Frontend.Pages
 {
     public class HU_01Model : PageModel
     {
+        private readonly IRepositorioPaciente repositorioPaciente;
+        [BindProperty]
+        public Paciente Paciente {get; set; }
         public void OnGet()
         {
+            Paciente = new Paciente();
+        }
+
+        public IActionResult OnPost()
+        {
+            
+            repositorioPaciente.AddPaciente(Paciente);
+            return Page();
         }
     }
 }
