f46e205c2c94adfd8528dd782e2e52f986540257
John betancur perez <johndavidbetancurperez@gmail.com>
2022-07-17 11:06:39 -0500

    se sube proyecto con conexión a base de datos

---
+++ HospiEnCasa.App/HospiEnCasa.App.Consola/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
+
+namespace HospiEnCasa.App.Consola
+{
+
+    class Program
+    {
+        private static IRepositorioPaciente _repoPaciente = new RepositorioPaciente(new Persistencia.AppContext());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Entity Framework");
+            //AddPaciente();
+            //BuscarPaciente(3);
+        }
+        public static void AddPaciente()
+        {
+            var paciente = new Paciente()
+            {
+                Nombre = "Pepito",
+                Apellidos = "Perez Prieto",
+                Telefono = "300000000",
+                Genero = Genero.masculino,
+                Direccion = "Calle 8",
+                Longitud = 9.54f,
+                Latitud = 45.55f,
+                Ciudad = "Santa Marta",
+                FechaNacimiento = new DateTime(1972, 11, 08)
+            };
+            _repoPaciente.AddPaciente(paciente);
+        }
+
+        private static void BuscarPaciente(int idPaciente)
+        {
+            var paciente = _repoPaciente.GetPaciente(idPaciente);
+            Console.WriteLine(paciente.Nombre + " " + paciente.Apellidos + " " + paciente.Genero);
+        }
+    }
+}
---
+++ HospiEnCasa.App/HospiEnCasa.App.Dominio/Entidades/Medico.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HospiEnCasa.App.Dominio
+{
+    public class Medico:Persona
+    {
+        string Especialidad {get;set;}
+        string Codigo {get;set;}
+        string RegistroRethus {get;set;}
+    }
+}
