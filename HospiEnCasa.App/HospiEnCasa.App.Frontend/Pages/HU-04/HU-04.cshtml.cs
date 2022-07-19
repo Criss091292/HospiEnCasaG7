cf0805534a916082e9e7a401ea42d6108aca2e33
Tania Vasquez <tvvasquezg@unal.edu.co>
2022-07-18 20:09:24 -0500

    Se añade listado pacientes y boton adicionar paciente

---
+++ HospiEnCasa.App/HospiEnCasa.App.Frontend/Pages/HU-04/HU-04.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Persistencia;
+using HospiEnCasa.App.Dominio;
+
+namespace HospiEnCasa.App.Frontend
+{
+    public class HU_04Model : PageModel
+    {
+        private readonly IRepositorioPaciente repositorioPacientes;
+        public IEnumerable<Paciente> Pacientes {get;set;}
+        public HU_04Model(IRepositorioPaciente repositorioPacientes)
+        {
+            this.repositorioPacientes=repositorioPacientes;
+        }
+        public void OnGet()
+        {
+            Pacientes = repositorioPacientes.GetAllPacientes();
+        }
+    }
+}
