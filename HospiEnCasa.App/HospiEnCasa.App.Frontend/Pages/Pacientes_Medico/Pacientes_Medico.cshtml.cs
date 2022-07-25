49f5b5893724c706aa6cf7ac81a0cd557092082c
John betancur perez <johndavidbetancurperez@gmail.com>
2022-07-24 21:40:18 -0500

    Se corrige errores

--- HospiEnCasa.App/HospiEnCasa.App.Frontend/Pages/Pacientes_Medico/Pacientes_Medico.cshtml.cs
+++ HospiEnCasa.App/HospiEnCasa.App.Frontend/Pages/Pacientes_Medico/Pacientes_Medico.cshtml.cs
@@ -1,12 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Persistencia;
+using HospiEnCasa.App.Dominio;
 
 namespace MyApp.Namespace
 {
     public class Pacientes_MedicoModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioMedico repositorioMedico;
+        public IEnumerable<Medico> Medicos {get;set;}
+        public Medico Medico = new Medico();
+        public Pacientes_MedicoModel(IRepositorioMedico repositorioMedico)
         {
+            this.repositorioMedico = repositorioMedico;
+        }
+        public void OnGet(string medicoDocumento)
+        {
+            Medico = repositorioMedico.GetMedico(medicoDocumento);
         }
     }
 }
